d9cb1ee56a4411a92744a10f6477c986ee9011d0
Juancbr99 <103227579+Juancbr99@users.noreply.github.com>
2024-12-09 19:37:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+
+    tecla.onclick = function(){  
+        playSonido(idAudio);
+    };
+}
+
+    tecla.onkeydown = function(evento){
+
+        if( evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }
+        console.log(evento.code === 'Space' || evento.code === 'Enter')
+}
+
+    tecla.onkeydown = function(){
+        tecla.classList.remove('activa');
+}
+
+
+
+
+
+
+
+
+
+
